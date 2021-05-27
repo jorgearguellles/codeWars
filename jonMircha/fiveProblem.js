b5801176fceb68df8265952eb454ae85c507de2e
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-05-27 17:32:56 -0500

    add/ jonMircha fiveProblem.js is done

--- jonMircha/fiveProblem.js
+++ jonMircha/fiveProblem.js
@@ -2,3 +2,11 @@
 5) Programa una función que invierta las palabras de una cadena de texto,
 pe.miFuncion("Hola Mundo") devolverá "odnuM aloH".
 */
+
+function reverseString(str = "") {
+	!str
+		? console.warn("No ingresaste una cadena de texto")
+		: console.log(str.split("").reverse().join(""));
+}
+
+reverseString("Ayer");
