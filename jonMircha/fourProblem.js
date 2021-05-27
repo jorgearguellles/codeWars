56a7d9fe672ed7de954bb67fc6ab0106924ecb82
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-05-27 16:50:36 -0500

    update/ jonMicrha/fourProblem.js finish

--- jonMircha/fourProblem.js
+++ jonMircha/fourProblem.js
@@ -1,2 +1,21 @@
 /* 4) Programa una función que repita un texto X veces, 
 pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo. */
+
+const arrayString = [
+	"Hola buenos días ",
+	"Hola Sexy ",
+	"Jorge Alberto ",
+	"Colombia tierra querida ",
+	"Cali ciudad verde ",
+	"Buenos días ",
+	"Ayer si dormí ",
+];
+let indexRandom = Math.floor(Math.random() * arrayString.length);
+let arrayToWork = arrayString[indexRandom];
+
+function repeatStr(array, times) {
+	let srtR = array.repeat(times);
+	console.log(`Este texto "${array}" repetido ${times}, se ve así: ${srtR}`);
+}
+
+repeatStr(arrayToWork, 3);
