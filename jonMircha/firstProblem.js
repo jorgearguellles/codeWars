5bd5880eb07f2d0f747e537c0ef56a7ed161a1b5
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-05-27 09:53:48 -0500

    update/jonMircha/firstProblem.js done

--- jonMircha/firstProblem.js
+++ jonMircha/firstProblem.js
@@ -1,30 +1,50 @@
 /* 1) Programa una función que cuente el número de caracteres de una cadena de texto, 
 pe. miFuncion("Hola Mundo") devolverá 10. */
 
-let testRandom = Math.floor(Math.random() * 3);
+function generateDataTest() {
+	let indexRandom = Math.floor(Math.random() * 4);
 
-let generate = (testRandom) => {
-	let str = "Hola Sexy";
-	let number = 12345;
-	let array = [6, 7, 8, 9, 0];
-	let object = {
-		firstName: "Jorge",
-		lastName: "Arias",
-		city: "Cali",
-		country: "Colombia",
+	const arrayTest = [
+		"Hola Sexy",
+		10987,
+		[1, 2, 3, 4, 5, 6, 7],
+		{
+			firstName: "Jorge",
+			lastName: "Arias",
+			city: "Cali",
+			country: "Colombia",
+		},
+	];
+
+	let elementToTest = arrayTest[indexRandom];
+	return elementToTest;
+}
+
+function countCharacters(dataTest) {
+	Object.size = function (dataTest) {
+		let size = 0,
+			key;
+		for (key in dataTest) {
+			if (dataTest.hasOwnProperty(key)) size++;
+		}
+		return size;
 	};
 
-	if (testRandom === 1) {
-		console.log(str);
-	} else if (testRandom === 2) {
-		return number;
-	} else if (testRandom === 3) {
-		return array;
-	} else if (testRandom === 4) {
-		return object;
+	if (typeof dataTest === "string") {
+		console.log("Dato de prueba: ", dataTest, "Longitud: ", dataTest.length);
+	} else if (typeof dataTest === "number") {
+		console.log(
+			"Dato de prueba: ",
+			dataTest,
+			"Longitud: ",
+			dataTest.toString().length
+		);
+	} else if (typeof dataTest === "object") {
+		let size = Object.size(dataTest);
+		console.log("Dato de prueba: ", dataTest, "Longitud: ", size);
+	} else {
+		console.log("Dato de prueba: ", dataTest, "Longitud: ", dataTest.length);
 	}
-};
-
-function countCharacters(generate) {
-	return generate;
 }
+
+countCharacters(generateDataTest());
