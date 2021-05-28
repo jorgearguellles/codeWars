3a422f0b31520cbed0a476d10bcbd58ab4d19cf9
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-05-28 11:15:21 -0500

    add/ jonMircha nineProblem.js is done

--- jonMircha/nineProblem.js
+++ jonMircha/nineProblem.js
@@ -1,3 +1,9 @@
 /*
 9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
 */
+
+function getRandomArbitrary(min, max) {
+	console.log(Math.floor(Math.random() * (max - min) + min));
+}
+
+getRandomArbitrary(501, 600);
