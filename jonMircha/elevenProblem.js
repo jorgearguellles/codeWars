cc2cf0a5a3436ec915c9727666a39eb378eeac06
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2021-06-21 16:52:33 -0500

    add/ jonMircha/ ten & eleven problems resolves

--- jonMircha/elevenProblem.js
+++ jonMircha/elevenProblem.js
@@ -1,5 +1,27 @@
 /*
-11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, 
+11) Programa una función que calcule el factorial de un número 
+(El factorial de un entero positivo n, 
   se define como el producto de todos los números enteros positivos desde 1 hasta n),
 pe. miFuncion(5) devolverá 120.
  */
+
+const factorial = (numero = undefined) => {
+  if(numero === undefined) return console.warn(`No ingresaste un número`);
+  if(typeof numero !== "number") return console.error(`El valor ${numero} ingresado no es un número`);
+  if(numero === 0) return console.error(`El número no puede ser 0`);
+  if(Math.sign(numero) === -1) return console.error(`El número no puede ser negativo`);
+  
+  let factorial = 1;
+
+  for (let index = numero; index > 1; index--){
+    factorial = factorial * index;
+  }
+  return console.log(`El factorial del número ${numero} es igual a: ${factorial}`)
+}
+
+factorial();
+factorial("1");
+factorial(0);
+factorial(-2);
+factorial(5);
+
--- jonMircha/tenProblem.js
+++ jonMircha/tenProblem.js
@@ -2,21 +2,23 @@
 10) Programa una función que reciba un número y evalúe si es capicúa o no 
 (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
 */
-let numToSeparate = 200;
 
-let arr = Array.from(String(numToSeparate), Number);
-console.log(arr);
+/*  Respuesta de Jon */
 
-function arrayEquals(numOrdered, numReversed) {
-	for (let i = 0; i < numOrdered.length; ++i) {
-		if (numOrdered[i] !== numReversed[i])
-			return console.log(
-				`El texto "${numToSeparate}" no es Palíndromo: ${numReversed.join("")}`
-			);
-	}
-	return console.log(
-		`El texto "${numToSeparate}" si es Palíndromo: ${numReversed.join("")}`
-	);
+const capicua = (numero) => {
+	if(!numero) return console.warn("No ingresaste un número");
+
+	if(typeof numero !== "number") return console.error(`El valor ${numero} ingresado no es un número.`)
+
+	numero = numero.toString();
+	let alReves = numero.split("").reverse().join("");
+
+	return( numero === alReves)
+		? console.info(`Sí es capicúa porque el número ingresado es el mismo al derecho ${numero} y al reves ${alReves}`)
+		: console.info(`No es capicúa porque el número ingresado no es el mismo al derecho ${numero} y al reves ${alReves}`);
 }
 
-arrayEquals(arr, arr.reverse());
+capicua();
+capicua("2");
+capicua(200);
+capicua(202);
