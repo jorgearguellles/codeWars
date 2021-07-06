12143787fe7f61fc1120906c13521557c4519d40
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2021-07-06 16:26:09 -0500

    add/ fizzBuzz.js & cicloDeTriangulo.js & ajedreztable.js

---
+++ eloquentJS/ajedrezTable.js
@@ -0,0 +1,22 @@
+/*
+Escribe un programa que cree un string que represente una cuadrícula de 8 × 8, 
+usando caracteres de nueva línea para separar las líneas. 
+En cada posición de la cuadrícula hay un espacio o un carácter "#".
+Los caracteres deberían de formar un tablero de ajedrez.
+
+Pasar este string a console.log debería mostrar algo como esto:
+_#_#_#_#
+#_#_#_#_
+_#_#_#_#
+#_#_#_#_
+_#_#_#_#
+#_#_#_#_
+_#_#_#_#
+#_#_#_#_
+
+Cuando tengas un programa que genere este patrón, define una vinculación tamaño = 8 y 
+cambia el programa para que funcione con cualquier tamaño, dando como salida una 
+cuadrícula con el alto y ancho dados.
+*/
+
+console.log("ene \n jsjsj" );
