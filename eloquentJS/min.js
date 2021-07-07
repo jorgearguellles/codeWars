973c146d992a2226c96237d9ed6c0f5ce12366a6
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2021-07-07 11:34:35 -0500

    add/ minimu.js

---
+++ eloquentJS/min.js
@@ -0,0 +1,12 @@
+/*
+ * El capítulo anterior introdujo la función estándar Math.min 
+ * que devuelve su argumento más pequeño. 
+ * Nosotros podemos construir algo como eso ahora. 
+ * Escribe una función min que tome dos argumentos y retorne su mínimo.
+ */
+
+function min(a,b){
+  console.log(Math.min(a,b))
+};
+
+min(2,3);
